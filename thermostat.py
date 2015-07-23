f77329a208b141ceebd358ab3f7a4974cbc2ca1b
AmirKhalili <akhalili@gmail.com>
2015-07-23 01:24:19 -0700

    saving the authorization to config.txt

--- thermostat.py
+++ thermostat.py
@@ -2,6 +2,7 @@ __author__ = 'Amirhossein'
 
 import json
 import requests
+import datetime
 
 # Consts
 ECOBEE_URL = 'https://api.ecobee.com/'
@@ -11,7 +12,11 @@ class Thermostat:
     def __init__(self, api_key):
 
         self.api_key = api_key
-        self.authorize()
+
+        self.readConfigFile()
+
+        if datetime.datetime.now() > self.expires:
+            self.authorize()
 
     # authorize the thermostart
     def authorize(self):
@@ -28,19 +33,37 @@ class Thermostat:
 
 
         # Request access and refresh tokens
-        token_params = {'grant_type': u'ecobeePin', 'code': self.authorization_token, 'client_id': self.api_key}
+        token_params = {'grant_type':'ecobeePin', 'code': self.authorization_token, 'client_id': self.api_key}
         data = self.post('token', params=token_params)
         self.accesss_token = data['access_token']
         self.token_type = data['token_type']
         self.refresh_token = data['refresh_token']
-        self.expires_in = data['expires_in']
+        self.expires = datetime.datetime.now() + datetime.timedelta(minutes=data['expires_in'])
 
-        with open('./config.txt','w') as config_file:
+
+        with open('./config.txt','wb') as config_file:
+            data = {'access_token': self.accesss_token, 'token_type': self.token_type, 'refresh_token': self.refresh_token, 'expires': self.expires.strftime('%Y-%m-%d %H:%M:%S')}
             json.dump(data, config_file, ensure_ascii=False)
 
+    def readConfigFile(self):
 
-    # Communicate with ecobee using the url
-    # url: the url to communicate with ecobee
+        data = []
+        with open('./config.txt','rb') as config_file:
+            data = json.load(config_file)
+
+        self.accesss_token = data['access_token']
+        self.token_type = data['token_type']
+        self.refresh_token = data['refresh_token']
+        self.expires = datetime.datetime.strptime(data['expires'],'%Y-%m-%d %H:%M:%S')
+
+
+        return data
+
+
+
+    # get a task and parameters to ecobee
+    # task: the task
+    # params: parameters to send
     # returns: json parsed response
     def get(self, task, params):
 
@@ -51,13 +74,14 @@ class Thermostat:
         return parsed_response
 
 
-    # Communicate with ecobee using the url
-    # url: the url to communicate with ecobee
+    # post a task and parameters to ecobee
+    # task: the task
+    # params: parameters to send
     # returns: json parsed response
     def post(self, task, params):
 
-        url = ECOBEE_URL + task + '?grant_type=ecobeePin&code=' + self.authorization_token + '&client_id=' + self.api_key
-        response = requests.post(url)
+        url = ECOBEE_URL + task
+        response = requests.post(url, params=params)
         parsed_response = response.json()
 
         return parsed_response
