09750909f7c9fab4ee3c84af9a922a6b7c021af9
AmirKhalili <akhalili@gmail.com>
2015-07-22 22:30:06 -0700

    fixing authorization

--- thermostat.py
+++ thermostat.py
@@ -19,20 +19,24 @@ class Thermostat:
         # Request authorization
         authorization_params = {'response_type': 'ecobeePin', 'client_id': self.api_key, 'scope': 'smartWrite'}
         data = self.get('authorize', params=authorization_params)
-        self.pin_code = data['ecobeePin']
+        pin_code = data['ecobeePin']
         expired_mins = data['expires_in']
+        self.authorization_token = data['code']
 
-        print('In ' + str(expired_mins) +' Please register the app with pin code ' + self.pin_code + ' in your ecobee portal at http:////www.ecobee.com')
+        print('In ' + str(expired_mins) +' Please register the app with pin code ' + pin_code + ' in your ecobee portal at http:////www.ecobee.com')
         raw_input('Press enter to continue...')
 
 
         # Request access and refresh tokens
-        token_params = {'grant_type': u'ecobeePin', 'code': self.pin_code, 'client_id': self.api_key}
+        token_params = {'grant_type': u'ecobeePin', 'code': self.authorization_token, 'client_id': self.api_key}
         data = self.post('token', params=token_params)
         self.accesss_token = data['access_token']
         self.token_type = data['token_type']
         self.refresh_token = data['refresh_token']
         self.expires_in = data['expires_in']
+
+        with open('./config.txt','w') as config_file:
+            json.dump(data, config_file, ensure_ascii=False)
 
 
     # Communicate with ecobee using the url
@@ -52,9 +56,8 @@ class Thermostat:
     # returns: json parsed response
     def post(self, task, params):
 
-        url = ECOBEE_URL + task + '?grant_type=refresh_token&code=' + self.pin_code + '&client_id=' + self.api_key
+        url = ECOBEE_URL + task + '?grant_type=ecobeePin&code=' + self.authorization_token + '&client_id=' + self.api_key
         response = requests.post(url)
-        print(response.url)
         parsed_response = response.json()
 
         return parsed_response
