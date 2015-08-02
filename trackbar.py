6715c0dcb0a942a12519c0fc07c8e3755f78deff
AmirKhalili <akhalili@gmail.com>
2015-08-01 22:14:06 -0700

    testing the slider bar

--- trackbar.py
+++ trackbar.py
@@ -24,7 +24,8 @@ class TrackBar(Widget):
 
                 # Add the marker
                 Color(1, 1, 0)
-                marker_diameter = abs(marker_idx - self.__num_markers / 2) * height_factor / 4 + 3
+                marker_diameter = (self.__num_markers- abs(marker_idx - self.__num_markers / 2)) * height_factor / 5 + 3
                 Ellipse(pos=(self.x + center_x - marker_diameter / 2, self.y + marker_idx*height_factor),
                         size=(marker_diameter, marker_diameter)
                         )
+                
